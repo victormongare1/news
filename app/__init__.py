aafdd366ce5f4b23eabb9c095912b632180b9d4b
victormongare1 <vicmongz254@gmail.com>
2020-04-25 16:00:17 +0300

    created blueprint

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+bootstrap=Bootstrap()
+def create_app(config_name):
+  app = Flask(__name__)
+  #creating app configurations
+  app.config.from_object(config_options[config_name])
+  #initializing flask extensions
+  bootstrap.init_app(app)
+  return app
