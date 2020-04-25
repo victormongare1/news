4cceb148e280d6dac9abc6a6c8bce3d7736260da
victormongare1 <vicmongz254@gmail.com>
2020-04-25 16:25:24 +0300

    added loops to index.html

---
+++ app/main/views.py
@@ -0,0 +1,12 @@
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..request import get_source
+#views
+@main.route('/')
+def index():
+  '''
+  view root that returns the index page and various news sources 
+  '''
+  title = 'MY CURRENT NEWS'
+  sources=get_source('sources')
+  return render_template('index.html',title=title,sources=sources)
--- manage.py
+++ manage.py
@@ -1,3 +1,3 @@
-from app import app
-if __name__ == "__main__":
-  app.run()  
+from app import create_app
+from flask_script import Manager,Server
+#
