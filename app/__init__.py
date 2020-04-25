c34ceb0eacf7aec6c5d93a4f32d978f741a9363f
victormongare1 <vicmongz254@gmail.com>
2020-04-25 16:07:27 +0300

    added setting configuration

--- app/__init__.py
+++ app/__init__.py
@@ -11,5 +11,8 @@ def create_app(config_name):
   #blueprint register
   from .main import main as main_blueprint
   app.register_blueprint(main_blueprint)
-  
+  #setting config
+  from .request import configure_request
+  configure_request(app)
+
   return app
