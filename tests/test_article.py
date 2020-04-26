ca95ed3ebec965026f4c8709f8d02939c215588d
victormongare1 <vicmongz254@gmail.com>
2020-04-26 19:24:04 +0300

    changed webpage styling

--- tests/test_article.py
+++ tests/test_article.py
@@ -8,8 +8,7 @@ class ArticleTest(unittest.TestCase):
     '''
     setup method that will run before every test
     '''
-    self.newarticle=ARTICLE("mike","news today",
-    "today","content")
+    self.newarticle=ARTICLE("mike","news today","today","content")
   def test_instance(self):
     '''
     test to assert whether newsource is an instance of the source object
